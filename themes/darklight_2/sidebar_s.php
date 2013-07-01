581c995315448d686f3e4d55a668dc1763e36c2e
xiebiao <joyrap@gmail.com>
2013-07-02 00:03:18 +0800

    去掉搜索框中的默认值

--- themes/darklight_2/sidebar_s.php
+++ themes/darklight_2/sidebar_s.php
@@ -9,7 +9,7 @@
 	 <h2>SEARCH THIS BLOG</h2>
       <div id="sidebarsearch">
         <form id="searchform" method="get" action="<?php bloginfo('home'); ?>">
-          <input type="text" name="s" id="s" size="18" onfocus="if(this.value=='Input keywords to search') this.value='';" onblur="if(this.value=='') this.value='Input keywords to search';" value="Input keywords to search"/>
+          <input type="text" name="s" id="s" size="18" onfocus="if(this.value=='') this.value='';" onblur="if(this.value=='') this.value='Input keywords to search';" value="Input keywords to search"/>
           <input id="searchsubmit" name="sbutt" type="submit" value="" alt="Submit"/>
         </form>
       </div>
