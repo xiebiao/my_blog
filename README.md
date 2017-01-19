f316c94d9c47d49b1e43503be8eb3e109d5e37aa
bjxieb <xiebiao@jd.com>
2017-01-19 17:51:11 +0800

    rebuild

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-my_blog
-=======
+Homepage
+------------------
 
-Wordpress
+## template
+
+http://www.missyuan.net/school/other_2014/other_13712.html
+http://sc.chinaz.com/info/150216296456.htm
--- themes/darklight_2/searchform.php
+++
@@ -1,6 +0,0 @@
-<form method="get" id="searchform" action="">
-  <div>
-    <input type="text" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" />
-    <input type="submit" id="searchsubmit" value="Search" />
-  </div>
-</form>
